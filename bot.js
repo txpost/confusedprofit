2ce6efbb9443c39abd6513c5643cd3dc7d8a8687
Trevor Postma <txpost@hotmail.com>
2014-12-14 22:26:11 -0400

    get antonym

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ getAntonym = function (word) {
 	var wordnikKey = process.env.WORDNIK_KEY;
 	var antonymURL = "http://api.wordnik.com:80/v4/word.json/" + word + "/relatedWords?useCanonical=true&relationshipTypes=antonym&limitPerRelationshipType=10&api_key=" + wordnikKey;
 
-	request(antonymURL, function (error, response, body) {
+	var antonym = request(antonymURL, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var json = JSON.parse(body);
 			// console.log(json[0].words);
@@ -38,6 +38,8 @@ getAntonym = function (word) {
 			console.log("there was a problem with the antonym" + error);
 		};
 	})
+
+	return antonym;
 }
 
 
@@ -55,7 +57,7 @@ getAdjectives = function (cb) {
 				var adjective = json[i].word;
 				// console.log(adjective);
 				var antonym = getAntonym(adjective);
-				console.log("the antonym for " + adjective + " is: " + antonym);
+				console.log(adjective + " is: " + antonym);
 				// if (antonym != undefined) {
 				// 	console.log("the antonym for " + adjective + " is: " + antonym);
 				// };
